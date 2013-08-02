6020dcc69b9b09891970b386d8c39b85a8dfcf82
Ric Allinson <capecodehq@yahoo.co.uk>
2013-08-01 18:05:37 -0700

    Update to tests

--- response.php
+++ response.php
@@ -342,7 +342,8 @@ class Response {
             };
         }
 
-        $type = "." . explode(".", $filename, 2)[1];
+        $type = "." . explode(".", $filename, 2);
+        $type = $type[1];
 
         if (!isset($this->renderer[$type])) {
             throw new \Exception("Renderer for type [" . $type . "] not set");
